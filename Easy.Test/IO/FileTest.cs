40df779fe25ccac28892312536a848ee0ca0af1a
Vlad Riscutia <riscutiavlad@gmail.com>
2013-06-18 16:06:41 -0700

    File utilities

---
+++ Easy.Test/IO/FileTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+using Windows.Storage;
+
+using Easy.IO;
+
+namespace Easy.Test.IO
+{
+    // Tests the File class
+    [TestClass]
+    public class FileTest
+    {
+        // Test unique temp files
+        [TestMethod]
+        public void TestGetTemp()
+        {
+            var tempFiles = new HashSet<string>();
+
+            // Generate 20 temp files
+            for (int i = 0; i < 20; i++)
+            {
+                var fileTask = File.CreateTemporaryFileAsync();
+                fileTask.Wait();
+    
+                // File name should not have been previously generated
+                Assert.IsFalse(tempFiles.Contains(fileTask.Result.Name));
+
+                // Add to hash set
+                tempFiles.Add(fileTask.Result.Name);
+            }
+        }
+    }
+}
---
+++ Easy/IO/File.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.Foundation;
+using Windows.Storage; 
+
+namespace Easy.IO 
+{
+    /// <summary>
+    /// File utilities
+    /// </summary>
+    public class File
+    {
+        /// <summary>
+        /// Creates a unique temporary file
+        /// </summary>
+        /// <returns>Unique temporary file</returns>
+        public static async Task<IStorageFile> CreateTemporaryFileAsync()
+        {
+            // Generate GUID to use as unique name
+            string uniqueName = Guid.NewGuid().ToString("N");
+
+            // Create file in app's temporary folder
+            return await ApplicationData.Current.TemporaryFolder.CreateFileAsync(uniqueName);
+        }
+
+        /// <summary>
+        /// Performs a transactional file save
+        /// </summary>
+        /// <param name="file">File</param>
+        /// <param name="content">Content</param>
+        public static async void SaveAsync(IStorageFile file, string content)
+        {
+            var temp = await CreateTemporaryFileAsync();
+            await FileIO.WriteTextAsync(temp, content);
+            await temp.MoveAndReplaceAsync(file);
+        }
+
+        /// <summary>
+        /// Performs a transactional file save
+        /// </summary>
+        /// <param name="file">File</param>
+        /// <param name="buffer">Buffer</param>
+        public static async void SaveAsync(IStorageFile file, byte[] buffer)
+        {
+            var temp = await CreateTemporaryFileAsync();
+            await FileIO.WriteBytesAsync(temp, buffer);
+            await temp.MoveAndReplaceAsync(file);
+        }
+    }
+} 
