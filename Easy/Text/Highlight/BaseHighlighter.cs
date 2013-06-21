e11270a58c119478fde61db44bbb9358bdbcc939
Vlad Riscutia <riscutiavlad@gmail.com>
2013-06-21 16:27:43 -0700

    Syntax highlighter (work in progress)

---
+++ Easy/Text/Highlight/BaseHighlighter.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+using Windows.UI.Text;
+
+namespace Easy.Text.Highlight
+{
+    /// <summary>
+    /// Base syntax highlighting class
+    /// </summary>
+    public abstract class BaseHighlighter
+    {
+        /// <summary>
+        /// Text range to use in highlighting
+        /// </summary>
+        protected ITextRange workerRange;
+
+
+        /// <summary>
+        /// ITextDocument to highlight
+        /// </summary>
+        protected ITextDocument document;
+
+        // List of regexes to run on the document and corresponding character formats
+        private List<Tuple<Regex, ITextCharacterFormat>> _highlights;
+
+        /// <summary>
+        /// Creates a new instance of BaseHighlighter
+        /// </summary>
+        /// <param name="document">Document to highlight</param>
+        public BaseHighlighter(ITextDocument document)
+        {
+            this.document = document;
+
+            // Much faster to keep a worker range rather than to keep requesting one from
+            // the document
+            this.workerRange = document.GetRange(0, 0);
+
+            this._highlights = new List<Tuple<Regex, ITextCharacterFormat>>();
+
+            // Add highlighting rules
+            AddHighlightRules();          
+        }
+
+        /// <summary>
+        /// Adds a highlighting rule
+        /// </summary>
+        /// <param name="regex">Regular expression</param>
+        /// <param name="format">Associated format</param>
+        protected void AddHighlightRule(Regex regex, ITextCharacterFormat format)
+        {
+            _highlights.Add(new Tuple<Regex, ITextCharacterFormat>(regex, format));
+        }
+
+        /// <summary>
+        /// Adds a highlighting rule
+        /// </summary>
+        /// <param name="pattern">Regular expression pattern</param>
+        /// <param name="format">Associated format</param>
+        protected void AddHighlightRule(string pattern, ITextCharacterFormat format)
+        {
+            _highlights.Add(new Tuple<Regex, ITextCharacterFormat>(new Regex(pattern), format));
+        }
+
+        /// <summary>
+        /// Deriving classes should implement this to add the highlighting rules
+        /// </summary>
+        protected abstract void AddHighlightRules();
+
+        /// <summary>
+        /// Selects the text range to highlight
+        /// </summary>
+        protected virtual void SelectRange()
+        {
+            // Default is whole document
+            workerRange.SetRange(0, 0);
+            workerRange.Expand(TextRangeUnit.Story);
+        }
+
+        /// <summary>
+        /// Performs the highlighting
+        /// </summary>
+        public virtual void Highlight()
+        {
+            // Get the text from the worker range
+            string text;
+            SelectRange();
+            workerRange.GetText(TextGetOptions.None, out text);
+
+            // Remember start position of the worker range
+            int start = workerRange.StartPosition;
+
+            // For each highlighting rule
+            foreach (var highlight in _highlights)
+            {
+                // For each regex match
+                foreach (Match match in highlight.Item1.Matches(text))
+                {
+                    // Select the match and format it
+                    workerRange.StartPosition = workerRange.StartPosition + match.Index;
+                    workerRange.EndPosition = workerRange.StartPosition + match.Length;
+                    workerRange.CharacterFormat = highlight.Item2;
+                }
+            }
+        }
+    }
+}
---
+++ Easy/Text/Highlight/Markdown.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.UI.Text;
+
+namespace Easy.Text.Highlight
+{
+    /// <summary>
+    /// Simple Markdown highlighter
+    /// </summary>
+    class Markdown : BaseHighlighter
+    {
+        /// <summary>
+        /// Bold character format
+        /// </summary>
+        protected ITextCharacterFormat Bold;
+
+        /// <summary>
+        /// Italic character format
+        /// </summary>
+        protected ITextCharacterFormat Italic;
+
+        /// <summary>
+        /// Bold-italic character format
+        /// </summary>
+        protected ITextCharacterFormat BoldItalic;
+
+        /// <summary>
+        /// Creates a new Markdown highlighter
+        /// </summary>
+        /// <param name="document"></param>
+        public Markdown(ITextDocument document)
+            : base(document)
+        {
+            InitializeFormats();
+        }
+
+        /// <summary>
+        /// Initialize highlighting formats
+        /// </summary>
+        public void InitializeFormats()
+        {
+            // Initialize formats
+            Bold = document.GetDefaultCharacterFormat().GetClone();
+            Bold.Bold = FormatEffect.On;
+
+            Italic = document.GetDefaultCharacterFormat().GetClone();
+            Italic.Italic = FormatEffect.On;
+
+            BoldItalic = document.GetDefaultCharacterFormat().GetClone();
+            BoldItalic.Bold = FormatEffect.On;
+            BoldItalic.Italic = FormatEffect.On;
+        }
+
+        /// <summary>
+        /// Adds highlighting rules for Markdown
+        /// </summary>
+        protected override void AddHighlightRules()
+        {
+            // Heading with underline
+            AddHighlightRule(@"(^([^\r]+?)|\r([^\r]+?))[ ]*\r(=+|-+)[ ]*\r", Bold);
+
+            // Heading prefixed with pound
+            AddHighlightRule(@"(^(\#{1,6})|\r(\#{1,6}))[ ]*(.+?)[ ]*\#*\r", Bold);
+
+            // Bold text
+            AddHighlightRule(@"(\*\*|__)(?=\S)(.+?[*_]*)(?<=\S)\1", Bold);
+
+            // Italic text
+            AddHighlightRule(@"(\*|_)(?=\S)(.+?)(?<=\S)\1", Italic);
+
+            // Bold-italic text
+            AddHighlightRule(@"(\*\*\*|___)(?=\S)(.+?[*_]*)(?<=\S)\1", BoldItalic);
+        }
+    }
+}
